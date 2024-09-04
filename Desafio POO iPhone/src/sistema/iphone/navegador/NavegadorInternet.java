50ae0675c3b61f3269b4a2edf89d57740177d4e7
090Raphae <couto_@outlook.com>
2024-09-04 19:52:16 -0300

    news

---
+++ Desafio POO iPhone/src/sistema/iphone/navegador/NavegadorInternet.java
@@ -0,0 +1,9 @@
+package sistema.iphone.navegador;
+
+public interface  NavegadorInternet {
+
+    public void exibirPagina(String url);
+    public void adicionarNovaAba(String url);
+    public void atualizarPagina();
+    
+}
---
+++ Desafio POO iPhone/src/sistema/iphone/reprodutor/ReprodutorMusical.java
@@ -0,0 +1,10 @@
+package sistema.iphone.reprodutor;
+
+public interface ReprodutorMusical {
+
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+
+
+}
