a6d554919a41aa2e03c5155b4e1254bc6e5137cd
090Raphae <couto_@outlook.com>
2024-09-04 15:12:22 -0300

    Atualizacao

---
+++ Diagramação de classes iPhone/src/interface/Teclado.java
@@ -0,0 +1,11 @@
+public interface Teclado {
+    // Digita um caractere
+    void digitar(char caractere);
+
+    // Apaga o último caractere digitado
+    void apagar();
+
+    // Alterna o idioma do teclado
+    void alternarIdioma(String idioma);
+}
+
