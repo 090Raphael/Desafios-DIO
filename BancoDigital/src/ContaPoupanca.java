0b79aabaf1163d2f83ff1f094d6eb3b645be4f12
090Raphae <couto_@outlook.com>
2024-09-10 22:42:18 -0300

    Banco

---
+++ BancoDigital/src/ContaPoupanca.java
@@ -0,0 +1,15 @@
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int agencia, int numero, Cliente cliente) {
+        super(agencia, numero, cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        System.out.println("Agencia: " + this.getAgencia());
+        System.out.println("Número: " + this.getNumero());
+        System.out.println("Saldo: " + this.getSaldo());
+    }
+}
+
