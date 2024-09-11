e47ffd938afd9b541e582bb1063e577bbaaeeafa
090Raphae <couto_@outlook.com>
2024-09-11 12:40:59 -0300

    Upadate

---
+++ BancoDigital/src/src/Conta.java
@@ -0,0 +1,54 @@
+package src;
+
+public abstract class Conta {
+    protected double saldo;
+    protected int agencia;
+    protected int numero;
+    protected Cliente cliente;
+
+    public Conta(int agencia, int numero, Cliente cliente) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.cliente = cliente;
+        this.saldo = 0.0;
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            this.saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        if (saldo >= valor) {
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+        } else {
+            System.out.println("Saldo insuficiente para transferência!");
+        }
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public abstract void imprimirExtrato(); // Método abstrato para ser implementado nas subclasses
+}
