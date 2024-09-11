37bcbe5a8447b110b54a78e2513ffc1096f5ef8c
090Raphae <couto_@outlook.com>
2024-09-11 16:04:13 -0300

    Desafio abstrair bootcamp Entregue

---
+++ Desafio abstrair bootcamp/src/main/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso de Java");
+        cursoJava.setDescricao("Aprenda Java na prática");
+        cursoJava.setCargaHoraria(8);
+
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria sobre Java");
+        mentoriaJava.setDescricao("Discussão sobre boas práticas em Java");
+        mentoriaJava.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp para aprender Java do zero");
+        bootcamp.getConteudos().add(cursoJava);
+        bootcamp.getConteudos().add(mentoriaJava);
+
+        Dev devRaphael = new Dev();
+        devRaphael.setNome("Raphael");
+        devRaphael.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Raphael: " + devRaphael.getConteudosInscritos());
+        
+        devRaphael.progredir();
+        System.out.println("Conteúdos concluídos Raphael: " + devRaphael.getConteudosConcluidos());
+        System.out.println("XP: " + devRaphael.calcularTotalXp());
+    }
+}
