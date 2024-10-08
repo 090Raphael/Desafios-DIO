ffcaeadfa65255e081cf4f892cad52f59e24eba4
090Raphae <couto_@outlook.com>
2024-10-08 16:46:48 -0300

    Alteração de nomes

---
+++ Desafios Entregues/dev-spring-2024/src/main/java/me/dio/dev_spring_2024/DevSpring2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.dev_spring_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpring2024Application.class, args);
+	}
+
+}
---
+++ Desafios Entregues/dev-spring-2024/src/test/java/me/dio/dev_spring_2024/DevSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dev_spring_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
